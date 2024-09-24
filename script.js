6f4a045af5c0897291f5c4f7bed9d669bb0987fb
Jamie <79608519+jamiealonzo@users.noreply.github.com>
2024-09-23 21:51:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onresize = () => {
+    if (window.innerWidth > 768) {  // You can adjust the width based on your media queries
+        navbar.classList.remove('active');
+        menuIcon.classList.remove('bx-x');
+    }
+}
+
+// scroll section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    });
+}
